0e8e3350cd93f7862d5639047601d04e94b584fc
uj0e <dhqxlajtm040@naver.com>
2025-01-20 16:02:50 +0900

    first upload

---
+++ src/main/java/org/advisor/MemberDataApplication.java
@@ -0,0 +1,13 @@
+package org.advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberDataApplication.class, args);
+	}
+
+}
